a57b7af50465e805ca5b66b6c88bc1c29678d543
harshaTejaR <40051202+harshaTejaR@users.noreply.github.com>
2022-08-24 13:24:56 +0530

    Header addd

---
+++ frontend_dev/src/Components/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+// import Scope from "../Basics/Scope";
+// import Function from "../Basics/Function";
+// import Destructure from "../Basics/Destructuring";
+import Header from "./UI/MainHeader";
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      {/* <Scope />
+      <Function />
+      <Destructure /> */}
+    </>
+  );
+}
+
+export default App;
